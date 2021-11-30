e07ab8920da8ae490ea803614b0af681783882b3
unknown <isabella14brandao@gmail.com>
2021-11-30 15:01:13 -0300

    projeto finalizado

---
+++ OneDrive/Ambiente de Trabalho/segundo-projeto/segundo-projeto/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Title from './Componentes/Title/Title';
+import Input from './Componentes/Input/Input';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Title>Meu Segundo Projeto React do Zero</Title>
+        <Input/>
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
